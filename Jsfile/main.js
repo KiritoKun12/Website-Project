f2249a67bd06cc46cc1608c794a4d7fa3e6709d2
Harsh Bagadia <65998581+KiritoKun12@users.noreply.github.com>
2020-05-31 07:40:23 +0530

    Create main.js

---
+++ Jsfile/main.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyALzbf9Ui9IWV8lfPG4Z6PQYYv04CW8WNs",
+    authDomain: "myproject1-99999.firebaseapp.com",
+    databaseURL: "https://myproject1-99999.firebaseio.com",
+    projectId: "myproject1-99999",
+    storageBucket: "myproject1-99999.appspot.com",
+    messagingSenderId: "916024972112",
+    appId: "1:916024972112:web:7204ef3ab3d055a27fd255",
+    measurementId: "G-G1Y2TN1GFL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
